91ee63a2f8430fae6ff4e1d31932f5f7cf778f76
Akshay Shukla <akshayshukla1032@gmail.com>
2022-12-09 19:22:53 +0530

    Hello

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amber,
+        appBar:AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.brown,
+          ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
